331c5947fa914ed9099bc403088ad846e4ac034b
luoyujie <1478219870@qq.com>
2026-07-16 16:36:03 +0800

    用户注册和token

--- routers/users.py
+++ routers/users.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException,status
 from sqlalchemy.ext.asyncio import AsyncSession
+
 from config.db_conf import get_db
 from schemas.users import UserRequest
+from crud import users
 
 
 router = APIRouter(prefix="/api/user",tags=["user"])
@@ -10,16 +12,24 @@ router = APIRouter(prefix="/api/user",tags=["user"])
 @router.post("/register")
 async def register(user_data:UserRequest,db:AsyncSession = Depends(get_db)):  #用户信息和db
     #用户注册
+    #注册逻辑：验证用户是否存在->创建用户->生成token->响应结果
+    existing_user = await users.get_user_by_username(db, user_data.username)
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="用户已经存在")
+    user = await users.create_user(db,user_data)
+    
+    token = await users.create_token(db,user.id)
+
     return {
     "code": 200,
     "message": "注册成功",
     "data": {
-    "token": "用户访问令牌",
+    "token": token,
     "userInfo": {
-      "id": 1,
-      "username": user_data.username,
-      "bio": "这个人很懒，什么都没留下",
-      "avatar": "https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
+      "id": user.id,
+      "username": user.username,
+      "bio": user.bio,
+      "avatar": user.avatar
     }
   }
 }
