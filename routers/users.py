7250364033ec8982301d87bbef91ed75a744298e
luoyujie <1478219870@qq.com>
2026-07-20 10:50:14 +0800

    查询用户信息+更新用户信息

--- routers/users.py
+++ routers/users.py
@@ -4,9 +4,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.responses import JSONResponse
 
 from config.db_conf import get_db
-from schemas.users import UserRequest, UserAuthResponse, UserInfoResponse
+from models.users import User
+from schemas.users import UserRequest, UserAuthResponse, UserInfoResponse, UserUpdateRequest
 from crud import users
 from utils.response import success_response
+from utils.auth import get_current_user
 
 router = APIRouter(prefix="/api/user",tags=["user"])
 
@@ -49,3 +51,19 @@ async def login(user_data:UserRequest,db:AsyncSession = Depends(get_db)):
     token = await users.create_token(db,user.id)
     response_data = UserAuthResponse(token = token,user_info = UserInfoResponse.model_validate(user))
     return success_response(message="登陆成功",data=response_data)
+
+
+#查token查用户->封装crud->功能整合成一个工具函数->路由导入使用:依赖注入
+@router.get("/info")
+async def get_user_info(user:User = Depends(get_current_user)):
+    return success_response(message="获取用户信息成功",data = UserInfoResponse.model_validate(user))
+
+
+
+#修改用户信息：验证token->更新(用户输入数据put提交 -> 请求体参数->定义Pydantic模型类)->响应结果
+#参数：用户输入的+验证token+db（调用更新的方法）
+@router.put("/update")
+async def update_user_info(user_data:UserUpdateRequest,user:User = Depends(get_current_user),
+                           db:AsyncSession = Depends(get_db)):
+    user = await users.update_user(db,user.username,user_data)
+    return success_response(message="更新用户信息成功",data=UserInfoResponse.model_validate(user))
