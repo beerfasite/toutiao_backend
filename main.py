fe6df7c896fca8cfd86b974f18e0ce21fc26e09c
luoyujie <1478219870@qq.com>
2026-07-21 10:36:39 +0800

    查看收藏状态

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import news,users
+from routers import news,users,favorite
 from utils.exception_handlers import register_exception_handlers
 
 # 创建 FastAPI 实例
@@ -37,3 +37,4 @@ async def root():
 #挂载路由/注册路由
 app.include_router(news.router)
 app.include_router(users.router)
+app.include_router(favorite.router)
