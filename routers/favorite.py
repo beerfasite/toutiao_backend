5aea609556094f7dcb26f3d4fe846e503f3f32d1
luoyujie <1478219870@qq.com>
2026-07-21 16:26:25 +0800

    添加收藏

--- routers/favorite.py
+++ routers/favorite.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from config.db_conf import get_db
 from models.users import User
-from schemas.favorite import FavoriteCheckResponse
+from schemas.favorite import FavoriteCheckResponse, FavoriteAddRequest
 from utils.auth import get_current_user
 from utils.response import success_response
 from crud import favorite
@@ -19,3 +19,21 @@ async def check_favorite(
 
     is_favorite = await favorite.is_news_favorite(db,user.id,news_id)
     return success_response(message="检查收藏状态成功",data = FavoriteCheckResponse(isFavorite=is_favorite))
+
+
+
+@router.post("/add")
+async def add_favorite(
+        data:FavoriteAddRequest,
+        user:User = Depends(get_current_user),
+        db:AsyncSession = Depends(get_db)
+):
+    result = await favorite.add_news_favorite(db,user.id,data.news_id)
+    return success_response(message="添加收藏成功",data=result)
+
+
+
+
+
+
+
--- schemas/favorite.py
+++ schemas/favorite.py
@@ -6,6 +6,8 @@ class FavoriteCheckResponse(BaseModel):
     is_favorite: bool = Field(..., alias="isFavorite")
 
 
+class FavoriteAddRequest(BaseModel):
+    news_id: int = Field(..., alias="newsId")
 
 
 
