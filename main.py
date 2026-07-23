02426ed5d1e3bec47019dd8527777db545973c55
luoyujie <1478219870@qq.com>
2026-07-23 10:50:47 +0800

    收藏模块完成

--- main.py
+++ main.py
@@ -1,40 +1,30 @@
 from fastapi import FastAPI
+from routers import news, users, favorite
 from fastapi.middleware.cors import CORSMiddleware
-from routers import news,users,favorite
+
 from utils.exception_handlers import register_exception_handlers
 
-# 创建 FastAPI 实例
 app = FastAPI()
 
-
-#注册异常处理器
+# 注册异常处理器
 register_exception_handlers(app)
 
 
-#允许的来源
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:3000"
-]
-
-#允许的源，前端源，开发阶段允许所有源
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],        #允许的源，前端源，开发阶段允许所有源
-    allow_credentials=True,     #允许携带cookie
-    allow_methods=["*"],        #允许的请求方法
-    allow_headers=["*"],        #允许的请求头
+    allow_origins=["*"],     # 允许的源，开发阶段允许所有源，生产环境需要指定源
+    allow_credentials=True,  # 允许携带cookie
+    allow_methods=["*"],     # 允许的请求方法
+    allow_headers=["*"],     # 允许的请求头
 )
-
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World888"}
+    return {"message": "Hello World"}
 
-
-#挂载路由/注册路由
+# 挂载路由/注册路由
 app.include_router(news.router)
 app.include_router(users.router)
 app.include_router(favorite.router)
+
--- routers/users.py
+++ routers/users.py
@@ -1,33 +1,30 @@
-from fastapi import APIRouter, Depends, HTTPException,status
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette.responses import JSONResponse
+from starlette import status
 
-from config.db_conf import get_db
 from models.users import User
 from schemas.users import UserRequest, UserAuthResponse, UserInfoResponse, UserUpdateRequest, UserChangePasswordRequest
+
+from config.db_conf import get_db
 from crud import users
 from utils.response import success_response
 from utils.auth import get_current_user
 
-router = APIRouter(prefix="/api/user",tags=["user"])
+router = APIRouter(prefix="/api/user", tags=["users"])
 
 
 @router.post("/register")
-async def register(user_data:UserRequest,db:AsyncSession = Depends(get_db)):  #用户信息和db
-    #用户注册
-    #注册逻辑：验证用户是否存在->创建用户->生成token->响应结果
+async def register(user_data: UserRequest, db: AsyncSession = Depends(get_db)):  # 用户信息 和 db
+    # 注册逻辑：验证用户是否存在 -> 创建用户 → 生成 Token  → 响应结果
     existing_user = await users.get_user_by_username(db, user_data.username)
     if existing_user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="用户已经存在")
-    user = await users.create_user(db,user_data)
-    
-    token = await users.create_token(db,user.id)
-
-    #     return {
-    #     "code": 200,
-    #     "message": "注册成功",
-    #     "data": {
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="用户已存在")
+    user = await users.create_user(db, user_data)
+    token = await users.create_token(db, user.id)
+    # return {
+    #   "code": 200,
+    #   "message": "注册成功",
+    #   "data": {
     #     "token": token,
     #     "userInfo": {
     #       "id": user.id,
@@ -37,45 +34,42 @@ async def register(user_data:UserRequest,db:AsyncSession = Depends(get_db)):  #�
     #     }
     #   }
     # }
-    response_data = UserAuthResponse(token = token,user_info = UserInfoResponse.model_validate(user))
-    return success_response(message="注册成功",data=response_data)
-
+    response_data = UserAuthResponse(token=token, user_info=UserInfoResponse.model_validate(user))
+    return success_response(message="注册成功", data=response_data)
 
 
 @router.post("/login")
-async def login(user_data:UserRequest,db:AsyncSession = Depends(get_db)):
-    #登录逻辑：验证用户存在->验证密码->生成Token->响应结果
-    user = await users.authenticate_user(db,user_data.username,user_data.password)
+async def login(user_data: UserRequest, db: AsyncSession = Depends(get_db)):
+    # 登录逻辑：验证用户是否存在 -> 验证密码 -> 生成 Token  → 响应结果
+    user = await users.authenticate_user(db, user_data.username, user_data.password)
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="用户名或密码错误")
-    token = await users.create_token(db,user.id)
-    response_data = UserAuthResponse(token = token,user_info = UserInfoResponse.model_validate(user))
-    return success_response(message="登陆成功",data=response_data)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="用户名或密码错误")
+    token = await users.create_token(db, user.id)
+    response_data = UserAuthResponse(token=token, user_info=UserInfoResponse.model_validate(user))
+    return success_response(message="登录成功啦", data=response_data)
 
 
-#查token查用户->封装crud->功能整合成一个工具函数->路由导入使用:依赖注入
+# 查Token查用户 → 封装crud → 功能整合成一个工具函数 → 路由导入使用: 依赖注入
 @router.get("/info")
-async def get_user_info(user:User = Depends(get_current_user)):
-    return success_response(message="获取用户信息成功",data = UserInfoResponse.model_validate(user))
+async def get_user_info(user: User = Depends(get_current_user)):
+    return success_response(message="获取用户信息成功", data=UserInfoResponse.model_validate(user))
 
 
-
-#修改用户信息：验证token->更新(用户输入数据put提交 -> 请求体参数->定义Pydantic模型类)->响应结果
-#参数：用户输入的+验证token+db（调用更新的方法）
+# 修改用户信息：验证Token → 更新（用户输入数据 put 提交 → 请求体参数 → 定义Pydantic模型类） → 响应结果
+# 参数：用户输入的 + 验证Token的 + db（调用更新的方法）
 @router.put("/update")
-async def update_user_info(user_data:UserUpdateRequest,user:User = Depends(get_current_user),
-                           db:AsyncSession = Depends(get_db)):
-    user = await users.update_user(db,user.username,user_data)
-    return success_response(message="更新用户信息成功",data=UserInfoResponse.model_validate(user))
+async def update_user_info(user_data: UserUpdateRequest, user: User = Depends(get_current_user),
+                           db: AsyncSession = Depends(get_db)):
+    user = await users.update_user(db, user.username, user_data)
+    return success_response(message="更新用户信息成功", data=UserInfoResponse.model_validate(user))
 
 
 @router.put("/password")
 async def update_password(
-        password_data:UserChangePasswordRequest,
-        user:User = Depends(get_current_user),
-        db:AsyncSession = Depends(get_db)):
-
-    res_change_pwd = await users.change_password(db,user,password_data.old_password,password_data.new_password)
+        password_data: UserChangePasswordRequest,
+        user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)):
+    res_change_pwd = await users.change_password(db, user, password_data.old_password, password_data.new_password)
     if not res_change_pwd:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="修改密码失败，请稍后再试")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="修改密码失败，请稍后再试")
     return success_response(message="修改密码成功")
